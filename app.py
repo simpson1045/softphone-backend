b0444d68dc97687aa83697d36486eca99345693f
simpson1045 <mchanington@cox.net>
2025-04-30 22:57:24 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,18 +5,18 @@ from voicemails import voicemail_bp
 
 app = Flask(__name__)
 
-@app.route("/call/incoming", methods=["POST"])
-def incoming_call():
-    print("📞 /call/incoming was hit!", flush=True)
-
-    resp = VoiceResponse()
-    resp.say("This is your test server. Twilio is connected.", voice="alice")
-    resp.hangup()
-
-    print("🔊 TwiML returned to Twilio:", flush=True)
-    print(str(resp), flush=True)
-
-    return Response(str(resp), mimetype="text/xml")
+#@app.route("/call/incoming", methods=["POST"])
+#def incoming_call():
+ #   print("📞 /call/incoming was hit!", flush=True)
+#
+   # resp = VoiceResponse()
+  #  resp.say("This is your test server. Twilio is connected.", voice="alice")
+ #   resp.hangup()
+#
+  #  print("🔊 TwiML returned to Twilio:", flush=True)
+ #   print(str(resp), flush=True)
+#
+   # return Response(str(resp), mimetype="text/xml")
 
 @app.route("/", methods=["GET"])
 def health_check():
